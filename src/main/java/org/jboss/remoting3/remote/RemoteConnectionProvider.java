7a0087b67935cbe3c6427b3a6b1595e4b6318947
Rico Neubauer <R.Neubauer@seeburger.de>
2017-11-06 10:13:56 +0100

    [Bug#78226] Hardcoding TCP keepalive as temporary fix

--- src/main/java/org/jboss/remoting3/remote/RemoteConnectionProvider.java
+++ src/main/java/org/jboss/remoting3/remote/RemoteConnectionProvider.java
@@ -167,27 +167,9 @@ final class RemoteConnectionProvider extends AbstractHandleableCloseable<Connect
             public void handleEvent(final ConnectedStreamChannel channel) {
                 try {
                     channel.setOption(Options.TCP_NODELAY, Boolean.TRUE);
-
-                    // set other TCP options in case they were set externally
-                    if (Boolean.TRUE.equals(connectOptions.get(Options.KEEP_ALIVE))) {
-                        if (RemoteLogger.log.isTraceEnabled()) {
-                            RemoteLogger.log.tracef("KEEP_ALIVE set for channel %s" + channel.getLocalAddress());
-                        }
-                        channel.setOption(Options.KEEP_ALIVE, Boolean.TRUE);
-                    }
-
-                    if (Boolean.TRUE.equals(connectOptions.get(Options.REUSE_ADDRESSES))) {
-                        if (RemoteLogger.log.isTraceEnabled()) {
-                            RemoteLogger.log.tracef("REUSE_ADDRESSES set for channel %s" + channel.getLocalAddress());
-                        }
-                        channel.setOption(Options.REUSE_ADDRESSES, Boolean.TRUE);
-                    }
-
-                    if (Boolean.TRUE.equals(connectOptions.get(Options.TCP_OOB_INLINE))) {
-                        if (RemoteLogger.log.isTraceEnabled()) {
-                            RemoteLogger.log.tracef("TCP_OOB_INLINE set for channel %s" + channel.getLocalAddress());
-                        }
-                        channel.setOption(Options.TCP_OOB_INLINE, Boolean.TRUE);
+                    channel.setOption(Options.KEEP_ALIVE, Boolean.TRUE);
+                    if (RemoteLogger.log.isTraceEnabled()) {
+                        RemoteLogger.log.tracef("KEEP_ALIVE and TCP_NODELAY set for channel %s" + channel.getLocalAddress());
                     }
                 } catch (IOException e) {
                     // ignore
@@ -348,12 +330,9 @@ final class RemoteConnectionProvider extends AbstractHandleableCloseable<Connect
             }
             try {
                 accepted.setOption(Options.TCP_NODELAY, Boolean.TRUE);
-
-                if (Boolean.TRUE.equals(serverOptionMap.get(Options.KEEP_ALIVE))) {
-                    if (RemoteLogger.log.isTraceEnabled()) {
-                        RemoteLogger.log.tracef("KEEP_ALIVE set for accepting channel %s" + accepted.getLocalAddress());
-                    }
-                    accepted.setOption(Options.KEEP_ALIVE, Boolean.TRUE);
+                accepted.setOption(Options.KEEP_ALIVE, Boolean.TRUE);
+                if (RemoteLogger.log.isTraceEnabled()) {
+                    RemoteLogger.log.tracef("KEEP_ALIVE and TCP_NODELAY set for accepting channel %s" + accepted.getLocalAddress());
                 }
             } catch (IOException e) {
                 // ignore
